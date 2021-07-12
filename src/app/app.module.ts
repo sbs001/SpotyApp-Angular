b3ebf755642ff95e9d6d4a16c2ce2642c109f7dd
Sebastian <tassone_af@yahoo.com>
2021-07-12 01:07:47 -0300

    create card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { SpotifyService } from './services/spotify.service';
 //pipes
 import { NoImgPipe } from './pipes/no-img.pipe';
 import { CardComponent } from './components/card/card.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { CardComponent } from './components/card/card.component';
     NavbarComponent,
     NoImgPipe,
     CardComponent,
+    LoadingComponent,
   ],
   imports: [
     BrowserModule, 
